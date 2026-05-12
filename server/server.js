2fedac88132b83aefc46750f8156fd352958b82b
aka-name <ahsan.ethical@gmail.com>
2026-05-13 02:38:04 +0530

    resolved cors issues

--- server/server.js
+++ server/server.js
@@ -35,7 +35,15 @@ app.set('trust proxy', 1);
 app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' }
 }));
-app.use(cors({ origin: CLIENT_ORIGIN }));
+
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://neuralnet-social.vercel.app/'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(requestLogger);
